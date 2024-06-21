575d71e4a6aa91ebd4bddb81a5d441308e019787
D4KiR <23246564+d4kir92@users.noreply.github.com>
2024-06-21 13:51:44 +0200

    v1.0.80

--- libs/LIB_lang.lua
+++ libs/LIB_lang.lua
@@ -1,7 +1,6 @@
--- LIB Design
 local _, DRaidFrames = ...
+DRaidFrames:SetAddonOutput("DRaidFrames", 254652)
 local lang = {}
-
 function DRaidFrames:GetLangTab()
 	return lang
 end
@@ -9,13 +8,11 @@ end
 function DRaidFrames:GT(str, tab)
 	local strid = str
 	local result = DRaidFrames:GetLangTab()[strid]
-
 	if result ~= nil then
 		if tab ~= nil then
 			for i, v in pairs(tab) do
 				local find = i -- "[" .. i .. "]"
 				local replace = v
-
 				if find ~= nil and replace ~= nil then
 					result = string.gsub(result, find, replace)
 				end
@@ -30,7 +27,6 @@ end
 
 function DRaidFrames:UpdateLanguage()
 	DRaidFrames:DRFLangenUS()
-
 	if GetLocale() == "enUS" then
 		DRaidFrames:DRFLangenUS()
 	elseif GetLocale() == "deDE" then
