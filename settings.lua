0b28ee01847d7c223b04ce1808dc69b064f0d478
D4KiR <23246564+d4kir92@users.noreply.github.com>
2025-04-09 18:55:47 +0200

    v1.1.25

--- settings.lua
+++ settings.lua
@@ -61,7 +61,7 @@ end
 
 function DRaidFrames:InitSettings()
 	DRFTAB = DRFTAB or {}
-	DRaidFrames:SetVersion(254652, "1.1.24")
+	DRaidFrames:SetVersion(254652, "1.1.25")
 	drf_settings = DRaidFrames:CreateFrame(
 		{
 			["name"] = "DRaidFrames",
